f5bd1ed3c8ae5cdbb81aa5ab096b24b1e4f8b867
Bruno-Cerqueira <brunocerqueirasd@gmail.com>
2024-02-07 23:59:10 -0300

    chore: Config Prometheus client and server.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
+require 'prometheus/client'
+
 class ApplicationController < ActionController::Base
+  before_action :increment_request_count
+
+  def increment_request_count
+    counter = Prometheus::Client::Counter.new(:requests_total, 'Total number of requests')
+    counter.increment({ method: request.method, path: request.path })
+  end
 end
