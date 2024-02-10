a8c13fccf851a48975a0999ede6fb06a3e5f1254
Bruno-Cerqueira <brunocerqueirasd@gmail.com>
2024-02-09 23:54:28 -0300

    fix: Adjust counter controller call.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,10 @@ require 'prometheus/client'
 
 class ApplicationController < ActionController::Base
   before_action :increment_request_count
+  CONTROLLER_CALLS = Prometheus::Client::Counter.new(:controller_calls_total, docstring: 'Total number of controller calls', labels: [:method, :path])
 
   def increment_request_count
     CONTROLLER_CALLS.increment(labels: { method: request.method, path: request.path })
+    Prometheus::Client.registry.register(CONTROLLER_CALLS)
   end
 end
