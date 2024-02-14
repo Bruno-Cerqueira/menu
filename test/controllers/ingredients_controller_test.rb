1f0dbe93e896b4da9d6105458b5d75aa3bf473e7
Bruno-Cerqueira <brunocerqueirasd@gmail.com>
2024-02-13 23:59:56 -0300

    test: Create first unit test.

--- test/controllers/ingredients_controller_test.rb
+++ test/controllers/ingredients_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class IngredientsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    Ingredient.stub(:all, [ingredients(:ingredient1), ingredients(:ingredient2)]) do
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:ingredients)
+    end
+  end
 end
