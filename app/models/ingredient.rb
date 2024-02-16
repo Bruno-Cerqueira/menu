6c2a8a7d4ea70eb762b0d0887afa2b201b0e2e4d
Bruno-Cerqueira <brunocerqueirasd@gmail.com>
2024-02-15 22:39:10 -0300

    feat: Add validations for ingredient model.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,5 @@
 class Ingredient < ApplicationRecord
     has_many :ingredient_prices
+
+    validates :name, presence: true, uniqueness: true
 end
--- test/models/ingredient_test.rb
+++ test/models/ingredient_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class IngredientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be invalid without name" do
+    ingredient = Ingredient.new
+    
+    refute ingredient.valid?, "Ingredient is valid without a name"
+  end
+
+
+  test "should not save a ingredient with existing name" do
+    ingredient1 = Ingredient.new(name: "Cenoura")
+    ingredient2 = Ingredient.new(name: "Cenoura")
+    
+    refute ingredient2.valid?, "Ingredient is valid with duplicate name"
+  end
 end
