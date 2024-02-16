b2615538e5f89e0bc96ff842961118f9ed9a48d9
Bruno-Cerqueira <brunocerqueirasd@gmail.com>
2024-02-16 19:18:54 -0300

    fix: Adjust controller test. Install rails controller testing gem.

--- test/controllers/ingredients_controller_test.rb
+++ test/controllers/ingredients_controller_test.rb
@@ -1,11 +1,14 @@
 require "test_helper"
 
 class IngredientsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @ingredient1 = ingredients(:ingredient1)
+    @ingredient2 = ingredients(:ingredient2)
+  end
+
   test "should get index" do
-    Ingredient.stub(:all, [ingredients(:ingredient1), ingredients(:ingredient2)]) do
-      get :index
-      assert_response :success
-      assert_not_nil assigns(:ingredients)
-    end
+    get '/ingredients'
+    assert_response :success
+    assert_not_nil assigns(:ingredients)
   end
 end
