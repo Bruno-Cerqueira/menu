ff87e8ea8aeee0d6f7d6690e4ed9f9164f320f87
Bruno-Cerqueira <brunocerqueirasd@gmail.com>
2024-03-01 23:57:25 -0300

    feat: create root controller and route.

---
+++ test/controllers/homes_controller_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class HomesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "route home" do
+    assert_generates "/", :controller => "homes", :action => "index"
+  end
+
+end
