89564f5d51f417856e10fedb150bd7f0676745e7
Bruno-Cerqueira <brunocerqueirasd@gmail.com>
2024-03-27 23:55:27 -0300

    feat: Add available scope to ingredient model. Adjust tests with fixtures.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -6,4 +6,7 @@ class Ingredient < ApplicationRecord
   accepts_nested_attributes_for :ingredient_prices
 
   validates :name, presence: true, uniqueness: true
+
+  scope :available, -> { joins(:ingredient_quantities).where('ingredient_quantities.quantity > 0') }
+
 end
--- test/models/ingredient_test.rb
+++ test/models/ingredient_test.rb
@@ -1,15 +1,25 @@
 require 'test_helper'
 
 class IngredientTest < ActiveSupport::TestCase
-  test 'should be invalid without name' do
-    ingredient = Ingredient.new
-    refute ingredient.valid?, 'Ingredient is valid without a name'
+  def setup
+    @ingredient = ingredients(:ingredient1)
   end
 
-  test 'should not save a ingredient with existing name' do
-    ingredient1 = Ingredient.new(name: 'Cenoura')
+  test 'valid ingredient' do
+    assert @ingredient.valid?
+  end
+
+  test 'should be invalid without name' do
+    @ingredient.name = nil
+    refute @ingredient.valid?, 'Ingredient is valid without a name'
+    assert_not_nil @ingredient.errors[:name][0], 'no validation error for name present'
+  end
+
+  test 'should not save an ingredient with existing name' do
+    ingredient1 = @ingredient
     ingredient1.save
-    ingredient2 = Ingredient.new(name: 'Cenoura')
+    ingredient2 = Ingredient.new(name: @ingredient.name)
     refute ingredient2.valid?, 'Ingredient is valid with duplicate name'
+    assert_not_nil ingredient2.errors[:name][0], 'no validation error for name present'
   end
 end
